1beeba582cd7e4f082743f8121e404ded1eb1235
Rishi Yadav <rahulrishiyadav1994@gmail.com>
2024-09-14 17:28:40 +0530

    Update Calculator.py

--- Programs/Calculator.py
+++ Programs/Calculator.py
@@ -1,1 +1,17 @@
+num1 = float(input("Enter first number: "))
+op = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
 
+if op == '+':
+    print(num1 + num2)
+elif op == '-':
+    print(num1 - num2)
+elif op == '*':
+    print(num1 * num2)
+elif op == '/':
+    if num2 != 0:
+        print(num1 / num2)
+    else:
+        print("Error! Division by zero.")
+else:
+    print("Invalid operator")

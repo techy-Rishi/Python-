4be943ea89045cfbb5fdc1b73e9999d988feb122
Rishi Yadav <rahulrishiyadav1994@gmail.com>
2024-09-14 17:48:17 +0530

    Update Testing if a number is even or odd.py

--- Programs/Testing if a number is even or odd.py
+++ Programs/Testing if a number is even or odd.py
@@ -1,1 +1,10 @@
+a = int(input('Enter a number: '))
 
+def check_evod(num):
+    if num % 2 == 0:
+        return f'{num} is an even number'
+    else:
+        return f'{num} is an odd number'
+
+result = check_evod(a)
+print(result)
